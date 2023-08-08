725dceef39e41786ee2056d0ff6fcb41a3c1d90f
marti <martinaromeroacosta@gmail.com>
2023-08-07 22:06:27 -0300

    El div no saluda cuando se clickea el botón

--- eventos.js
+++ eventos.js
@@ -1,9 +1,11 @@
 function saludar(){
     alert("Hola!");
+    event.stopPropagation(); 
 }
 document.addEventListener("DOMContentLoaded", function () {
     let container = document.getElementsByTagName('div');
     container[0].addEventListener("click", function(){
         alert("Hola! Soy el div");
     });
+
 });
